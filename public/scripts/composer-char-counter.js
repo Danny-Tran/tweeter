906500221e8255e5e5627b5a103ce85bc5ca66f6
Danny-Tran <truong_tran@live.com>
2019-05-14 21:04:06 +0000

    counter working

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    console.log("HELLO!")
+    
+    $("textarea").keydown(function(){
+        let count = 0
+        count = $("textarea").val().length
+        if (count > 140) {
+        $(".counter").text(140 - count).css({color: 'red'})            
+        } else {
+        $(".counter").text(140 - count).css({color: 'green'})
+        }
+        
+    })
+    
+    
+    
+    
+
+
+
+
+});
+
+
+
